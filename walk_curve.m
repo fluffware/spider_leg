3e7665c6117c15d7121ba61fe235c24f73dd0ee2
Simon Berg <ksb@fluffware.se>
2022-03-21 22:29:50 +0100

    Plotted walk cycle

--- walk_curve.m
+++ walk_curve.m
@@ -1,11 +1,12 @@
-global a alpha beta J h;
+global a alpha beta J h
 a = 1
 alpha = 2*pi/3
 beta = alpha - pi
 J= 6*pi*a/(beta^3 * alpha)
+t= [0:0.001:4*pi];
 t1 = [-alpha:0.001:alpha];
 t2 = [alpha:0.001:pi];
-t3 = [pi:.001:2*pi-alpha]
+t3 = [pi:.001:2*pi-alpha];
 h = 0.3
 
 function retval = f2(t)
@@ -23,5 +24,36 @@ function retval = f4(t)
   retval = 2*h/(beta^3)*t.^3 - 3*h/(beta^2)*t.^2 + h;
 endfunction
 
+function retval = f5(t)
+  retval = f4(-t);
+endfunction
 
-plot(t2, f2(t2-pi),t1, (t1)*a/alpha, t3, f3(t3-pi), t2, f4(t2-pi));
+
+function retval = fs(t)
+  global alpha a;
+  t = mod(t, 2*pi);
+  if (t < alpha)
+    retval = a/alpha*t;
+    elseif (t >= 2*pi - alpha)
+      retval = a/alpha*(t-2*pi);
+    elseif (t >= pi)
+      retval = f3(t-pi);
+    else
+      retval = f2(t-pi);
+  endif
+endfunction
+
+function retval = fh(t)
+  global alpha a;
+  t = mod(t, 2*pi);
+  if (t < alpha || t >= 2*pi - alpha)
+    retval = 0;
+  elseif (t >= pi)
+    retval = f5(t-pi);
+  else
+    retval = f4(t-pi);
+  endif
+endfunction
+
+
+plot(t,arrayfun(@fs,t), t, arrayfun(@fh,t), arrayfun(@fs,t), arrayfun(@fh,t));
